e05b244825da0c6405e0e81e04b358f53e026e07
lohner <lohner>
2002-08-08 03:44:14 +0000

    - initial import of 1.1.15 (essentially 1.1.14 version)

---
+++ src/legal.h
@@ -0,0 +1,26 @@
+/*
+    NNGS - The No Name Go Server
+    Copyright (C) 1995-1997  Erik Van Riper (geek@midway.com)
+    and John Tromp (tromp@daisy.uwaterloo.ca/tromp@cwi.nl)
+
+    Adapted from:
+    fics - An internet chess server.
+    Copyright (C) 1993  Richard V. Nash
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+*/
+
+#ifndef _LEGAL_H_
+#define _LEGAL_H_
+
+extern char legalNotice[];
+
+#endif /* _LEGAL_H */
