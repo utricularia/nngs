6bb0392e993e0f553ac3241f14fc0560f171d5e0
awdvk <awdvk>
2004-11-17 23:18:30 +0000

    more ac/am woes

--- missing.h
+++ missing.h
@@ -1,25 +1,30 @@
 #ifndef MISSING_H
 #define MISSING_H 1
+#include <stdio.h>
 
-#if (!HAVE_POPEN)
+#if ( !HAVE_FILENO )
+#ifndef fileno	/* Avoid trouble; (if) fileno() is a macro */
+extern int fileno(FILE *fp);
+#endif
+#endif
+
+#if ( !HAVE_POPEN )
 extern FILE *popen(const char *, const char*);
-extern int *pclose(FILE *fp);
+#endif
+#if ( !HAVE_PCLOSE )
+extern int pclose(FILE *fp);
 #endif
 
-#if (!HAVE_FILENO)
-extern int fileno(FILE *fp); /* should be in stdio ... */
-#endif
-
-#if (!HAVE_VSNPRINTF)
+#if ( !HAVE_VSNPRINTF )
 #define HAVE_SNPRINTF 0
 extern int vsnprintf(char *buff,size_t s, const char *format, va_list ap);
 #endif
 
-#if (!HAVE_SNPRINTF)
+#if ( !HAVE_SNPRINTF )
 extern int snprintf(char *buff,size_t s, const char *format, ...);
 #endif
 
-#if (!HAVE_FTRUNCATE)
+#if ( !HAVE_FTRUNCATE )
 #define off_t size_t
 extern int ftruncate(int fd, off_t pos);
 #undef off_t 
